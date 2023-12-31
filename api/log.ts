30349f8be698a4e06fe993a50c5277c31a20b429
Michael Jewell <michaeljewell9911@gmail.com>
2023-12-30 19:34:06 -0700

    mj - add logging

---
+++ api/log.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { pino } from "pino";
+
+export const logger = pino();
+
+function round(n: number, precision: number) {
+  const factor = Math.pow(10, precision);
+  return Math.round(n * factor) / factor;
+}
+
+function now() {
+  return typeof performance === "undefined" ? Date.now() : performance.now();
+}
+
+export function log(
+  callback: (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const start = now();
+    const params = {
+      method: req.method,
+      url: req.url,
+    };
+    logger.info({
+      ...params,
+      message: "Request start",
+    });
+
+    await callback(req, res);
+
+    const durationMs = now() - start;
+    logger.info({
+      ...params,
+      status: res.statusCode,
+      duration: `${round(durationMs, 2)}`,
+      message: "Request end",
+    });
+  };
+}
--- pages/api/[...ts-rest].tsx
+++ pages/api/[...ts-rest].tsx
@@ -1,8 +1,11 @@
 import { createNextRouter } from "@ts-rest/next";
 import { contract } from "~/api/contract";
 import { errorHandler } from "~/api/errors";
+import { log } from "~/api/log";
 import { router } from "~/api/router";
 
-export default createNextRouter(contract, router, {
-  errorHandler,
-});
+export default log(
+  createNextRouter(contract, router, {
+    errorHandler,
+  })
+);
