5e2a98b4e607a3fa693ff5259337eb3f8bdf191b
Michael Jewell <mjewell@ontra.ai>
2024-06-21 15:17:22 -0600

    add vitest

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -6,11 +6,14 @@ import * as Entities from "@/entities";
 import { Options } from "@mikro-orm/core";
 import { Migrator } from "@mikro-orm/migrations";
 import { defineConfig } from "@mikro-orm/postgresql";
+import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
 const config: Options = defineConfig({
+  metadataProvider: TsMorphMetadataProvider,
   entities: Object.values(Entities),
+  entitiesTs: Object.values(Entities),
   clientUrl: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?schema=public`,
   extensions: [Migrator],
   debug: process.env.LOG_QUERIES === "true",
